0b5c11e763456c7bee21414f8976a343dbce2c95
sufyan3 <sufyan@diyainteractive.com>
2023-12-04 00:29:00 +0500

    create arrays

---
+++ app.js
@@ -0,0 +1,142 @@
+// ARRAYS//
+
+// Q1
+
+// var student = [];
+// student.push ("ahmed");
+// student.push("ali");
+// student.push("sufyan");
+// student.push("anas")
+// ("welcome "+student[3])
+
+
+// Q2
+
+// var student = [];
+// student.push("ahmed","ali","shoaib","usman","hanzala");
+// console.log("welcome "+student[2]);
+
+
+// Q3
+
+// var name1 = ["john","smith","dom","kallis"];
+// console.log(name1[0])
+
+
+// Q4
+
+// var numbers = [23,56,78,67,45];
+// console.log(numbers[2])
+
+
+// Q5
+
+// var boolean = [true,false];
+// console.log(boolean[0])
+
+
+// Q6
+
+// var mixed = [1,"names,",false];
+// console.log(mixed[2])
+
+
+// Q7
+
+// var qualification = ["SSC","HSC","BSC","BS","BCOM","MS","MPHIL","PHD"];
+// var html = `
+// <h1>Qualifications:</h1>
+// <br>
+// <h2>1)${qualification[0]}<h2/>
+// <h2>1)${qualification[1]}<h2/>
+// <h2>1)${qualification[2]}<h2/>
+// <h2>1)${qualification[3]}<h2/>
+// <h2>1)${qualification[4]}<h2/>
+// <h2>1)${qualification[5]}<h2/>
+// <h2>1)${qualification[6]}<h2/>
+// <h2>1)${qualification[7]}<h2/>
+// `
+// document.write(html);
+
+
+// Q8
+
+// var students = ["john","michael","tony"];
+// var studentMarks = [320,230,480];
+// var totalMarks = 500;
+// console.log(studentMarks[0]/totalMarks*100)
+// document.write("<h2>Score of john is 320.Percentage is:64%<h2/>");
+// console.log(studentMarks[1]/totalMarks*100)
+// document.write("<h2>Score of michael is 230.Percentage is:46%<h2/>");
+// console.log(studentMarks[2]/totalMarks*100)
+// document.write("<h2>Score of john is 480.Percentage is:96%<h2/>");
+
+
+// Q9(a)
+
+// var colorsName = ["black","blue","pink","white","green","gray"];
+// document.write(colorsName);
+//  var colorBefore = prompt("what color do you want to add in the begining")
+//  colorsName.unshift(colorBefore)
+//  document.write("<br>")
+//  document.write(colorsName)
+
+// Q9(b)
+
+// var colorsName = ["black","blue","pink","white","green","gray"];
+// document.write(colorsName);
+//  var colorEnd = prompt("what color do you want to add in the end")
+//  colorsName.push(colorEnd)
+//  document.write("<br>")
+//  document.write(colorsName)
+
+// Q9(c)
+
+
+// var colorsName = ["black","blue","pink","white","green",];
+// document.write(colorsName);
+//  var colorBefore = prompt("what color do you want to add in the begining")
+//  colorsName.unshift(colorBefore)
+//  document.write("<br>")
+//  document.write(colorsName)
+
+// Q9(d)
+
+// var colorsName = ["black","blue","pink","white","green",];
+// document.write(colorsName);
+//  colorsName.shift([0])
+//  document.write("<br>")
+//  document.write(colorsName)
+
+// Q9(e)
+
+// var colorsName = ["black","blue","pink","white","green",];
+// document.write(colorsName);
+//  colorsName.pop([0])
+//  document.write("<br>")
+//  document.write(colorsName)
+
+
+// Qp(f)
+
+// var colorsName = ["black","blue","pink","white","green",];
+// document.write(colorsName);
+// var wantsColor = prompt("enter a color name which index");
+// colorsName.splice()
+// document.write("<br>")
+// document.write(colorsName)
+
+
+// Qno 10
+
+// var studentScore = [320,230,480,120];
+// document.write(studentScore);
+// studentScore.sort();
+// document.write("<br>")
+// document.write(studentScore);
+
+// Qno 12
+
+// var arr = ["this","is","my","cat"];
+// console.log(arr.join());
+// console.log(arr.join(''));
